7bd9f32cf5fa3fa176b789cffe7e7114a3348b76
Tom Christie <tom@tomchristie.com>
2019-04-04 18:46:12 +0100

    Version 0.0.2

--- httpcore/__init__.py
+++ httpcore/__init__.py
@@ -2,4 +2,4 @@ from .api import PoolManager, Response, request
 from .config import PoolLimits, SSLConfig, TimeoutConfig
 from .exceptions import ResponseClosed, StreamConsumed
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
