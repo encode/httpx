fcb2802fe4835a6b85887db6a30d5286755fe7cc
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2019-05-11 14:56:08 +0100

    Additional tests for SyncClient

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -1,4 +1,6 @@
 import asyncio
+import datetime
+import email.utils
 import functools
 
 import pytest
@@ -30,6 +32,22 @@ def test_get(server):
     assert response.status_code == 200
     assert response.content == b"Hello, world!"
     assert response.text == "Hello, world!"
+    assert response.protocol == "HTTP/1.1"
+    assert (
+        datetime.datetime.now(datetime.timezone.utc)
+        - email.utils.parsedate_to_datetime(response.headers["date"])
+    ) < datetime.timedelta(seconds=1)
+    expected_headers = httpcore.Headers(
+        {
+            "server": "uvicorn",
+            "content-type": "text/plain",
+            "transfer-encoding": "chunked",
+        }
+    )
+    for header, value in expected_headers.items():
+        assert value == response.headers[header]
+
+    assert repr(response) == "<SyncResponse(status_code=200)>"
 
 
 @threadpool
@@ -69,3 +87,46 @@ def test_raw_iterator(server):
     for chunk in response.raw():
         body += chunk
     assert body == b"Hello, world!"
+    response.close()  # TODO: should Response be available as context managers?
+
+
+@threadpool
+def test_options(server):
+    with httpcore.SyncClient() as http:
+        response = http.options("http://127.0.0.1:8000/")
+
+    assert response.status_code == 200
+    assert response.reason_phrase == "OK"
+
+
+@threadpool
+def test_head(server):
+    with httpcore.SyncClient() as http:
+        response = http.head("http://127.0.0.1:8000/")
+
+    assert response.status_code == 200
+    assert response.reason_phrase == "OK"
+
+
+@threadpool
+def test_put(server):
+    with httpcore.SyncClient() as http:
+        response = http.put("http://127.0.0.1:8000/", data=b"Hello, world!")
+    assert response.status_code == 200
+    assert response.reason_phrase == "OK"
+
+
+@threadpool
+def test_patch(server):
+    with httpcore.SyncClient() as http:
+        response = http.patch("http://127.0.0.1:8000/", data=b"Hello, world!")
+    assert response.status_code == 200
+    assert response.reason_phrase == "OK"
+
+
+@threadpool
+def test_delete(server):
+    with httpcore.SyncClient() as http:
+        response = http.delete("http://127.0.0.1:8000/")
+    assert response.status_code == 200
+    assert response.reason_phrase == "OK"
