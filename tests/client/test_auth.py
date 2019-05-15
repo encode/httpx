905f457cca7ab452da87b480b88af071305af61f
Tom Christie <tom@tomchristie.com>
2019-05-15 11:43:19 +0100

    Add test for custom auth

--- tests/client/test_auth.py
+++ tests/client/test_auth.py
@@ -26,12 +26,26 @@ class MockDispatch(Dispatcher):
         return Response(200, content=body, request=request)
 
 
-def test_auth():
+def test_basic_auth():
     url = "https://example.org/"
-    auth=('tomchristie', 'password123')
+    auth = ('tomchristie', 'password123')
 
     with Client(dispatch=MockDispatch()) as client:
         response = client.get(url, auth=auth)
 
     assert response.status_code == 200
     assert json.loads(response.text) == {'auth': 'Basic dG9tY2hyaXN0aWU6cGFzc3dvcmQxMjM='}
+
+
+def test_custom_auth():
+    url = "https://example.org/"
+
+    def auth(request):
+        request.headers['Authorization'] = 'Token 123'
+        return request
+
+    with Client(dispatch=MockDispatch()) as client:
+        response = client.get(url, auth=auth)
+
+    assert response.status_code == 200
+    assert json.loads(response.text) == {'auth': 'Token 123'}
