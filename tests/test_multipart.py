a3ef6b38c31349295acefc1c769cfe033b72df26
Tom Christie <tom@tomchristie.com>
2019-06-17 14:32:58 +0100

    Test compat with 3.6

--- tests/test_multipart.py
+++ tests/test_multipart.py
@@ -27,27 +27,41 @@ class MockDispatch(Dispatcher):
 
 def test_multipart():
     client = Client(dispatch=MockDispatch())
+
+    # Test with a single-value 'data' argument, and a plain file 'files' argument.
     data = {"text": "abc"}
     files = {"file": io.BytesIO(b"<file content>")}
     response = client.post("http://127.0.0.1:8000/", data=data, files=files)
+    assert response.status_code == 200
+
+    # We're using the cgi module to verify the behavior here, which is a
+    # bit grungy, but sufficient just for our testing purposes.
     boundary = response.request.headers["Content-Type"].split("boundary=")[-1]
     content_length = response.request.headers["Content-Length"]
     pdict = {"boundary": boundary.encode("ascii"), "CONTENT-LENGTH": content_length}
-    assert response.status_code == 200
     multipart = cgi.parse_multipart(io.BytesIO(response.content), pdict)
-    assert multipart["text"] == ["abc"]
+
+    # Note that the expected return type for text fields appears to differs from 3.6 to 3.7+
+    assert multipart["text"] == ["abc"] or multipart["text"] == [b"abc"]
     assert multipart["file"] == [b"<file content>"]
 
 
 def test_multipart_file_tuple():
     client = Client(dispatch=MockDispatch())
+
+    # Test with a list of values 'data' argument, and a tuple style 'files' argument.
     data = {"text": ["abc"]}
     files = {"file": ("name.txt", io.BytesIO(b"<file content>"))}
     response = client.post("http://127.0.0.1:8000/", data=data, files=files)
+    assert response.status_code == 200
+
+    # We're using the cgi module to verify the behavior here, which is a
+    # bit grungy, but sufficient just for our testing purposes.
     boundary = response.request.headers["Content-Type"].split("boundary=")[-1]
     content_length = response.request.headers["Content-Length"]
     pdict = {"boundary": boundary.encode("ascii"), "CONTENT-LENGTH": content_length}
-    assert response.status_code == 200
     multipart = cgi.parse_multipart(io.BytesIO(response.content), pdict)
-    assert multipart["text"] == ["abc"]
+
+    # Note that the expected return type for text fields appears to differs from 3.6 to 3.7+
+    assert multipart["text"] == ["abc"] or multipart["text"] == [b"abc"]
     assert multipart["file"] == [b"<file content>"]
