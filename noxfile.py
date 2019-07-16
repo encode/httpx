771c7171eff6c87e02ea45da3aebb5b640e34a11
Seth Michael Larson <sethmichaellarson@gmail.com>
2019-07-16 15:57:04 -0500

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,15 @@
+import os
+
 import nox
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 @nox.session(reuse_venv=True)
 def lint(session):
     source_files = ("http3/", "tests/", "setup.py", "noxfile.py")
 
-    session.install("autoflake", "black", "flake8", "isort", "mypy")
+    session.install("autoflake", "black", "flake8", "isort")
     session.run("autoflake", "--in-place", "--recursive", *source_files)
     session.run(
         "isort",
@@ -19,7 +23,21 @@ def lint(session):
         *source_files,
     )
     session.run("black", "--line-length=88", "--target-version=py36", *source_files)
+
+    check(session)
+
+
+@nox.session(reuse_venv=True)
+def check(session):
+    source_files = ("http3/", "tests/", "setup.py", "noxfile.py")
+
+    session.install("flake8", "mypy", "black")
+
+    session.run(
+        "black", "--check", "--line-length=88", "--target-version=py36", *source_files
+    )
     session.run("flake8", "--max-line-length=88", "--ignore=W503,E203", *source_files)
+    session.run("mypy", "http3", "--ignore-missing-imports", "--disallow-untyped-defs")
 
 
 @nox.session(reuse_venv=True)
@@ -33,8 +51,15 @@ def docs(session):
 def test(session):
     session.install("-r", "test-requirements.txt")
     session.install(".")
-
     session.run(
-        "pytest", "--ignore=venv", "--cov=tests", "--cov=http3", "--cov-report="
+        "coverage",
+        "run",
+        "--omit='*'",
+        "-m",
+        "pytest",
+        "--cov=http3",
+        "--cov=tests",
+        "--cov-fail-under=100",
+        "--cov-report=term-missing",
+        *session.posargs,
     )
-    session.run("coverage", "report", "--show-missing", "--fail-under=100")
