417cfc589b18f8dd281bf27bf721fb3f47a85e47
Seth Michael Larson <sethmichaellarson@gmail.com>
2019-07-19 07:33:55 -0500

    lint more

--- noxfile.py
+++ noxfile.py
@@ -30,9 +30,7 @@ def check(session):
 
     session.run("black", "--check", "--target-version=py36", *source_files)
     session.run("flake8", "--max-line-length=88", "--ignore=W503,E203", *source_files)
-    session.run(
-        "mypy", "http3", "--ignore-missing-imports", "--disallow-untyped-defs"
-    )
+    session.run("mypy", "http3", "--ignore-missing-imports", "--disallow-untyped-defs")
 
 
 @nox.session(reuse_venv=True)
