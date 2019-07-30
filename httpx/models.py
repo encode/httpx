34d5101272e74b90ef79a9a16ca42dd3f7cab9fd
Tom Christie <tom@tomchristie.com>
2019-07-30 10:36:13 +0100

    Handle primitive data types for queryparams

--- httpx/models.py
+++ httpx/models.py
@@ -33,14 +33,17 @@ from .utils import (
     is_known_encoding,
     normalize_header_key,
     normalize_header_value,
+    str_query_param
 )
+
+PrimitiveData = typing.Union[str, int, float, bool, type(None)]
 
 URLTypes = typing.Union["URL", str]
 
 QueryParamTypes = typing.Union[
     "QueryParams",
-    typing.Mapping[str, str],
-    typing.List[typing.Tuple[str, str]],
+    typing.Mapping[PrimitiveData, PrimitiveData],
+    typing.List[typing.Tuple[PrimitiveData, PrimitiveData]],
     str,
 ]
 
@@ -259,8 +262,8 @@ class QueryParams(typing.Mapping[str, str]):
         else:
             items = value.items()  # type: ignore
 
-        self._list = [(str(k), str(v)) for k, v in items]
-        self._dict = {str(k): str(v) for k, v in items}
+        self._list = [(str_query_param(k), str_query_param(v)) for k, v in items]
+        self._dict = {str_query_param(k): str_query_param(v) for k, v in items}
 
     def getlist(self, key: typing.Any) -> typing.List[str]:
         return [item_value for item_key, item_value in self._list if item_key == key]
