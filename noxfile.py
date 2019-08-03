066b467d8e3d84b5be80fbe143a2d219d92b0a39
Humberto Rocha <humrochagf@gmail.com>
2019-08-03 17:25:51 -0300

    Parse scripts into noxfile

--- noxfile.py
+++ noxfile.py
@@ -5,4 +5,29 @@ import nox
 def tests(session):
     session.install("-r", "requirements.txt")
     session.install(".")
-    session.run('pytest')
+    session.run(
+        "pytest",
+        "--ignore",
+        "venv",
+        "--cov",
+        "tests",
+        "--cov",
+        "httpx",
+        env={"PYTHONPATH": "."},
+    )
+    session.run("coverage", "report", "--show-missing", "--fail-under=100")
+
+
+@nox.session
+def lint(session):
+    session.install("-r", "requirements.txt")
+    session.run(
+        "flake8",
+        "--max-line-length=88",
+        "--ignore=W503,E203,B305",
+        "httpx",
+        "tests",
+        "setup.py",
+        "noxfile.py",
+    )
+    session.run("mypy", "httpx", "--ignore-missing-imports", "--disallow-untyped-defs")
