9c08046a940a1b3b0f29a05510faf588d10d25f8
Seth Michael Larson <sethmichaellarson@gmail.com>
2019-08-21 11:42:21 -0500

    Check if a cipher is set on TLS stream

--- tests/test_concurrency.py
+++ tests/test_concurrency.py
@@ -5,12 +5,12 @@ import pytest
 from httpx import AsyncioBackend, HTTPVersionConfig, SSLConfig, TimeoutConfig
 
 
-@pytest.mark.skipif(
+@pytest.mark.xfail(
     sys.version_info < (3, 7),
     reason="Requires Python 3.7+ for AbstractEventLoop.start_tls()",
 )
 @pytest.mark.asyncio
-async def test_https_get_with_ssl_defaults(https_server):
+async def test_start_tls_on_socket_stream(https_server):
     """
     See that the backend can make a connection without TLS then
     start TLS on an existing connection.
@@ -21,9 +21,11 @@ async def test_https_get_with_ssl_defaults(https_server):
 
     stream = await backend.connect("127.0.0.1", 8001, None, timeout)
     assert stream.is_connection_dropped() is False
+    assert stream.stream_writer.get_extra_info("cipher", default=None) is None
 
     stream = await backend.start_tls(stream, "127.0.0.1", ctx, timeout)
     assert stream.is_connection_dropped() is False
+    assert stream.stream_writer.get_extra_info("cipher", default=None) is not None
 
     await stream.write(b"GET / HTTP/1.1\r\n\r\n")
     assert (await stream.read(8192, timeout)).startswith(b"HTTP/1.1 200 OK\r\n")
