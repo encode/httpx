d7d65b3e9763206eacf9df7302b214b0a4256dad
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2019-08-22 12:26:42 +0100

    Redirect and BasicAuth dispatchers

--- httpx/client.py
+++ httpx/client.py
@@ -19,8 +19,10 @@ from .config import (
 )
 from .dispatch.asgi import ASGIDispatch
 from .dispatch.base import AsyncDispatcher, Dispatcher
+from .dispatch.basic_auth import BasicAuthDispatcher
 from .dispatch.connection_pool import ConnectionPool
 from .dispatch.threaded import ThreadedDispatcher
+from .dispatch.redirect import RedirectDispatcher
 from .dispatch.wsgi import WSGIDispatch
 from .exceptions import (
     HTTPError,
@@ -148,37 +150,20 @@ class BaseClient:
         timeout: TimeoutTypes = None,
         trust_env: bool = None,
     ) -> AsyncResponse:
-        if auth is None:
-            auth = self.auth
-
         url = request.url
-
         if url.scheme not in ("http", "https"):
             raise InvalidURL('URL scheme must be "http" or "https".')
 
-        if auth is None:
-            if url.username or url.password:
-                auth = HTTPBasicAuth(username=url.username, password=url.password)
-            elif self.trust_env if trust_env is None else trust_env:
-                netrc_login = get_netrc_login(url.authority)
-                if netrc_login:
-                    netrc_username, _, netrc_password = netrc_login
-                    auth = HTTPBasicAuth(
-                        username=netrc_username, password=netrc_password
-                    )
-
-        if auth is not None:
-            if isinstance(auth, tuple):
-                auth = HTTPBasicAuth(username=auth[0], password=auth[1])
-            request = auth(request)
+        dispatcher: AsyncDispatcher = self._resolve_dispatch_chain(
+            request, auth, allow_redirects,
+        )
 
         try:
-            response = await self.send_handling_redirects(
+            response = await dispatcher.send(
                 request,
                 verify=verify,
                 cert=cert,
                 timeout=timeout,
-                allow_redirects=allow_redirects,
             )
         except HTTPError as exc:
             # Add the original request to any HTTPError
@@ -193,143 +178,22 @@ class BaseClient:
 
         return response
 
-    async def send_handling_redirects(
+    def _resolve_dispatch_chain(
         self,
         request: AsyncRequest,
-        *,
-        cert: CertTypes = None,
-        verify: VerifyTypes = None,
-        timeout: TimeoutTypes = None,
+        auth: AuthTypes = None,
         allow_redirects: bool = True,
-        history: typing.List[AsyncResponse] = None,
-    ) -> AsyncResponse:
-        if history is None:
-            history = []
+    ) -> AsyncDispatcher:
+        dispatcher: AsyncDispatcher = RedirectDispatcher(next_dispatcher=self.dispatch, base_cookies=self.cookies, allow_redirects=allow_redirects)
+        if auth is None and (request.url.username or request.url.password):
+            dispatcher = BasicAuthDispatcher(next_dispatcher=dispatcher, username=request.url.username, password=request.url.password)
+        elif isinstance(auth, tuple):
+            dispatcher = BasicAuthDispatcher(next_dispatcher=dispatcher, username=auth[0], password=auth[1])
+        elif isinstance(auth, HTTPBasicAuth):
+            dispatcher = BasicAuthDispatcher(next_dispatcher=dispatcher, username=auth.username, password=auth.password)
 
-        while True:
-            # We perform these checks here, so that calls to `response.next()`
-            # will raise redirect errors if appropriate.
-            if len(history) > self.max_redirects:
-                raise TooManyRedirects(response=history[-1])
-            if request.url in (response.url for response in history):
-                raise RedirectLoop(response=history[-1])
+        return dispatcher
 
-            response = await self.dispatch.send(
-                request, verify=verify, cert=cert, timeout=timeout
-            )
-
-            should_close_response = True
-            try:
-                assert isinstance(response, AsyncResponse)
-                response.history = list(history)
-                self.cookies.extract_cookies(response)
-                history.append(response)
-
-                if allow_redirects and response.is_redirect:
-                    request = self.build_redirect_request(request, response)
-                else:
-                    should_close_response = False
-                    break
-            finally:
-                if should_close_response:
-                    await response.close()
-
-        if response.is_redirect:
-
-            async def send_next() -> AsyncResponse:
-                nonlocal request, response, verify, cert
-                nonlocal allow_redirects, timeout, history
-                request = self.build_redirect_request(request, response)
-                response = await self.send_handling_redirects(
-                    request,
-                    allow_redirects=allow_redirects,
-                    verify=verify,
-                    cert=cert,
-                    timeout=timeout,
-                    history=history,
-                )
-                return response
-
-            response.next = send_next  # type: ignore
-
-        return response
-
-    def build_redirect_request(
-        self, request: AsyncRequest, response: AsyncResponse
-    ) -> AsyncRequest:
-        method = self.redirect_method(request, response)
-        url = self.redirect_url(request, response)
-        headers = self.redirect_headers(request, url)
-        content = self.redirect_content(request, method, response)
-        cookies = self.merge_cookies(request.cookies)
-        return AsyncRequest(
-            method=method, url=url, headers=headers, data=content, cookies=cookies
-        )
-
-    def redirect_method(self, request: AsyncRequest, response: AsyncResponse) -> str:
-        """
-        When being redirected we may want to change the method of the request
-        based on certain specs or browser behavior.
-        """
-        method = request.method
-
-        # https://tools.ietf.org/html/rfc7231#section-6.4.4
-        if response.status_code == codes.SEE_OTHER and method != "HEAD":
-            method = "GET"
-
-        # Do what the browsers do, despite standards...
-        # Turn 302s into GETs.
-        if response.status_code == codes.FOUND and method != "HEAD":
-            method = "GET"
-
-        # If a POST is responded to with a 301, turn it into a GET.
-        # This bizarre behaviour is explained in 'requests' issue 1704.
-        if response.status_code == codes.MOVED_PERMANENTLY and method == "POST":
-            method = "GET"
-
-        return method
-
-    def redirect_url(self, request: AsyncRequest, response: AsyncResponse) -> URL:
-        """
-        Return the URL for the redirect to follow.
-        """
-        location = response.headers["Location"]
-
-        url = URL(location, allow_relative=True)
-
-        # Facilitate relative 'Location' headers, as allowed by RFC 7231.
-        # (e.g. '/path/to/resource' instead of 'http://domain.tld/path/to/resource')
-        if url.is_relative_url:
-            url = request.url.join(url)
-
-        # Attach previous fragment if needed (RFC 7231 7.1.2)
-        if request.url.fragment and not url.fragment:
-            url = url.copy_with(fragment=request.url.fragment)
-
-        return url
-
-    def redirect_headers(self, request: AsyncRequest, url: URL) -> Headers:
-        """
-        Strip Authorization headers when responses are redirected away from
-        the origin.
-        """
-        headers = Headers(request.headers)
-        if url.origin != request.url.origin:
-            del headers["Authorization"]
-            del headers["host"]
-        return headers
-
-    def redirect_content(
-        self, request: AsyncRequest, method: str, response: AsyncResponse
-    ) -> bytes:
-        """
-        Return the body that should be used for the redirect request.
-        """
-        if method != request.method and method == "GET":
-            return b""
-        if request.is_streaming:
-            raise RedirectBodyUnavailable(response=response)
-        return request.content
 
 
 class AsyncClient(BaseClient):
