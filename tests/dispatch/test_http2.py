a4b93b91c03239738f6c118570fdfa155e7fe11e
Florimond Manca <florimond.manca@gmail.com>
2019-08-23 14:04:21 +0200

    Add missing async test cases (#269)

--- tests/dispatch/test_http2.py
+++ tests/dispatch/test_http2.py
@@ -1,6 +1,8 @@
 import json
 
-from httpx import Client, Response
+import pytest
+
+from httpx import AsyncClient, Client, Response
 
 from .utils import MockHTTP2Backend
 
@@ -27,11 +29,37 @@ def test_http2_get_request():
     assert json.loads(response.content) == {"method": "GET", "path": "/", "body": ""}
 
 
+@pytest.mark.asyncio
+async def test_async_http2_get_request():
+    backend = MockHTTP2Backend(app=app)
+
+    async with AsyncClient(backend=backend) as client:
+        response = await client.get("http://example.org")
+
+    assert response.status_code == 200
+    assert json.loads(response.content) == {"method": "GET", "path": "/", "body": ""}
+
+
 def test_http2_post_request():
     backend = MockHTTP2Backend(app=app)
 
     with Client(backend=backend) as client:
         response = client.post("http://example.org", data=b"<data>")
+
+    assert response.status_code == 200
+    assert json.loads(response.content) == {
+        "method": "POST",
+        "path": "/",
+        "body": "<data>",
+    }
+
+
+@pytest.mark.asyncio
+async def test_async_http2_post_request():
+    backend = MockHTTP2Backend(app=app)
+
+    async with AsyncClient(backend=backend) as client:
+        response = await client.post("http://example.org", data=b"<data>")
 
     assert response.status_code == 200
     assert json.loads(response.content) == {
@@ -59,6 +87,25 @@ def test_http2_multiple_requests():
     assert json.loads(response_3.content) == {"method": "GET", "path": "/3", "body": ""}
 
 
+@pytest.mark.asyncio
+async def test_async_http2_multiple_requests():
+    backend = MockHTTP2Backend(app=app)
+
+    async with AsyncClient(backend=backend) as client:
+        response_1 = await client.get("http://example.org/1")
+        response_2 = await client.get("http://example.org/2")
+        response_3 = await client.get("http://example.org/3")
+
+    assert response_1.status_code == 200
+    assert json.loads(response_1.content) == {"method": "GET", "path": "/1", "body": ""}
+
+    assert response_2.status_code == 200
+    assert json.loads(response_2.content) == {"method": "GET", "path": "/2", "body": ""}
+
+    assert response_3.status_code == 200
+    assert json.loads(response_3.content) == {"method": "GET", "path": "/3", "body": ""}
+
+
 def test_http2_reconnect():
     """
     If a connection has been dropped between requests, then we should
@@ -70,6 +117,26 @@ def test_http2_reconnect():
         response_1 = client.get("http://example.org/1")
         backend.server.close_connection = True
         response_2 = client.get("http://example.org/2")
+
+    assert response_1.status_code == 200
+    assert json.loads(response_1.content) == {"method": "GET", "path": "/1", "body": ""}
+
+    assert response_2.status_code == 200
+    assert json.loads(response_2.content) == {"method": "GET", "path": "/2", "body": ""}
+
+
+@pytest.mark.asyncio
+async def test_async_http2_reconnect():
+    """
+    If a connection has been dropped between requests, then we should
+    be seemlessly reconnected.
+    """
+    backend = MockHTTP2Backend(app=app)
+
+    async with AsyncClient(backend=backend) as client:
+        response_1 = await client.get("http://example.org/1")
+        backend.server.close_connection = True
+        response_2 = await client.get("http://example.org/2")
 
     assert response_1.status_code == 200
     assert json.loads(response_1.content) == {"method": "GET", "path": "/1", "body": ""}
