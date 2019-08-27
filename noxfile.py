35b6e52a243e85bb8259034790b461a2c1946367
Seth Michael Larson <sethmichaellarson@gmail.com>
2019-08-27 11:41:01 -0500

    Simply PR

--- noxfile.py
+++ noxfile.py
@@ -36,5 +36,4 @@ def docs(session):
 @nox.session(python=["3.6", "3.7", "3.8"])
 def test(session):
     session.install("-r", "test-requirements.txt")
-    test_files = session.posargs
-    session.run("python", "-m", "pytest", *test_files)
+    session.run("python", "-m", "pytest", *session.posargs)
