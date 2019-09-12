0ca57981babe195ab36a1ccdcaf4cb3309489e7a
can <cansarigol@derinbilgi.com.tr>
2019-09-12 11:07:44 +0300

    fixed reading SSL_CERT method

--- httpx/utils.py
+++ httpx/utils.py
@@ -108,12 +108,14 @@ def get_netrc_login(host: str) -> typing.Optional[typing.Tuple[str, str, str]]:
 
 
 def get_ca_bundle_from_env() -> typing.Optional[str]:
-    ssl_path = Path(os.getenv("SSL_CERT_FILE", ""))
-    if ssl_path and ssl_path.is_file():
-        return str(ssl_path)
-    ssl_path = Path(os.getenv("SSL_CERT_DIR", ""))
-    if ssl_path and ssl_path.is_dir():
-        return str(ssl_path)
+    if "SSL_CERT_FILE" in os.environ:
+        ssl_file = Path(os.environ["SSL_CERT_FILE"])
+        if ssl_file.is_file():
+            return str(ssl_file)
+    if "SSL_CERT_DIR" in os.environ:
+        ssl_path = Path(os.environ["SSL_CERT_DIR"])
+        if ssl_path.is_dir():
+            return str(ssl_path)
     return None
 
 
