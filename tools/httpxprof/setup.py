8bbc8f98d78b418918158e83acd9966375d78780
florimondmanca <florimond.manca@gmail.com>
2019-09-21 18:24:19 +0200

    Register tools/ for linting

--- tools/httpxprof/setup.py
+++ tools/httpxprof/setup.py
@@ -1,9 +1,10 @@
+import typing
 from pathlib import Path
 
 from setuptools import setup
 
 
-def get_packages(package):
+def get_packages(package: str) -> typing.List[str]:
     return [str(path.parent) for path in Path(package).glob("**/__init__.py")]
 
 
