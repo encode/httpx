7f76a642a8e41bfdca5362105d28808548812fb0
Quentin Pradet <quentin.pradet@gmail.com>
2019-10-01 11:30:20 +0400

    Add AsyncIO and Trio trove classifiers (#416)

--- setup.py
+++ setup.py
@@ -66,6 +66,8 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Topic :: Internet :: WWW/HTTP",
+        "Framework :: AsyncIO",
+        "Framework :: Trio",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
