0eb6b3cfc02cb04b8f52cb30d913cbd3173b3abd
Thomas Grainger <tagrain@gmail.com>
2019-10-04 14:59:41 +0100

    test that httpx/py.typed shows up in sdist and whl

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,8 @@
+import pathlib
+import re
+import tarfile
+import zipfile
+
 import nox
 
 nox.options.stop_on_first_error = True
@@ -48,3 +53,36 @@ def docs(session):
 def test(session):
     session.install("-r", "test-requirements.txt")
     session.run("python", "-m", "pytest", *session.posargs)
+
+
+@nox.session(reuse_venv=True)
+def check_dist(session):
+    output = session.run("python", "setup.py", "sdist", "bdist_wheel", silent=True)
+    assert (
+        re.search(r"(?m)^copying httpx/py\.typed -> httpx-[^/]+/httpx$", output)
+        is not None
+    )
+    assert (
+        re.search(
+            r"(?m)^copying build/lib/httpx/py\.typed -> "
+            r"build/bdist\.[^/]+/wheel/httpx$",
+            output,
+        )
+        is not None
+    )
+    assert re.search(r"(?m)^adding 'httpx/py\.typed'$", output) is not None
+
+    def tgz_has_py_typed(p: pathlib.Path) -> bool:
+        with tarfile.open(p) as sdist:
+            return any(
+                re.match(r"httpx-[^/]+/httpx/py\.typed", member.name)
+                for member in sdist
+            )
+
+    def whl_has_py_typed(p: pathlib.Path) -> bool:
+        with zipfile.ZipFile(p) as whl:
+            return whl.getinfo("httpx/py.typed").file_size == 0
+
+    dist_dir = pathlib.Path("dist")
+    assert [tgz_has_py_typed(sdist) for sdist in dist_dir.glob("**/*.tar.gz")] == [True]
+    assert [whl_has_py_typed(whl) for whl in dist_dir.glob("**/*.whl")] == [True]
