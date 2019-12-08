a2e2579771515a4655befee798cfa7aa80b1a90c
Tom Christie <tom@tomchristie.com>
2019-12-08 18:20:29 +0000

    Drop TimeoutFlag

--- httpx/concurrency/base.py
+++ httpx/concurrency/base.py
@@ -26,38 +26,6 @@ def lookup_backend(
     raise RuntimeError(f"Unknown or unsupported concurrency backend {backend!r}")
 
 
-class TimeoutFlag:
-    """
-    A timeout flag holds a state of either read-timeout or write-timeout mode.
-
-    We use this so that we can attempt both reads and writes concurrently, while
-    only enforcing timeouts in one direction.
-
-    During a request/response cycle we start in write-timeout mode.
-
-    Once we've sent a request fully, or once we start seeing a response,
-    then we switch to read-timeout mode instead.
-    """
-
-    def __init__(self) -> None:
-        self.raise_on_read_timeout = False
-        self.raise_on_write_timeout = True
-
-    def set_read_timeouts(self) -> None:
-        """
-        Set the flag to read-timeout mode.
-        """
-        self.raise_on_read_timeout = True
-        self.raise_on_write_timeout = False
-
-    def set_write_timeouts(self) -> None:
-        """
-        Set the flag to write-timeout mode.
-        """
-        self.raise_on_read_timeout = False
-        self.raise_on_write_timeout = True
-
-
 class BaseSocketStream:
     """
     A socket stream with read/write operations. Abstracts away any asyncio-specific
@@ -73,7 +41,7 @@ class BaseSocketStream:
     ) -> "BaseSocketStream":
         raise NotImplementedError()  # pragma: no cover
 
-    async def read(self, n: int, timeout: Timeout, flag: typing.Any = None) -> bytes:
+    async def read(self, n: int, timeout: Timeout) -> bytes:
         raise NotImplementedError()  # pragma: no cover
 
     async def write(self, data: bytes, timeout: Timeout) -> None:
