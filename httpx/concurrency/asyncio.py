8a60a36b1a0820da32513cd9003879b739383a2d
florimondmanca <florimond.manca@gmail.com>
2019-12-14 15:27:49 +0100

    Add comment on why wait_closed() is not called

--- httpx/concurrency/asyncio.py
+++ httpx/concurrency/asyncio.py
@@ -161,6 +161,13 @@ class SocketStream(BaseSocketStream):
         return self.stream_reader.at_eof()
 
     async def close(self) -> None:
+        # NOTE: StreamWriter instances expose a '.wait_closed()' coroutine function,
+        # but using it has caused compatibility issues with certain sites in
+        # the past (see https://github.com/encode/httpx/issues/634), which is
+        # why we don't call it here.
+        # This is fine, though, because '.close()' schedules the actual closing of the
+        # stream, meaning that at best it will happen during the next event loop
+        # iteration, and at worst asyncio will take care of it on program exit.
         self.stream_writer.close()
 
 
