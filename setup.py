bb6ab205fe60fd9ae9a1465188540dbfe46cf048
Tom Christie <tom@tomchristie.com>
2019-12-20 15:14:30 +0000

    Upgrade h11 to support both 0.8 and 0.9 (#658)

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
         "certifi",
         "hstspreload",
         "chardet==3.*",
-        "h11==0.8.*",
+        "h11>=0.8,<0.10",
         "h2==3.*",
         "idna==2.*",
         "rfc3986>=1.3,<2",
