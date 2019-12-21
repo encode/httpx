162014a47b2b843d0b19afd5db4d0e395346b1b3
florimondmanca <florimond.manca@gmail.com>
2019-12-21 15:58:35 +0100

    Add docstring to 'async_environment' fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -38,7 +38,21 @@ ENVIRONMENT_VARIABLES = {
         pytest.param("trio", marks=pytest.mark.trio),
     ]
 )
-def async_environment(request) -> str:
+def async_environment(request: typing.Any) -> str:
+    """
+    Mark a test function to be run on both asyncio and trio.
+
+    Equivalent to having a pair of tests, each respectively marked with
+    '@pytest.mark.asyncio' and '@pytest.mark.trio'.
+
+    Intended usage:
+
+    ```
+    @pytest.mark.usefixtures("async_environment")
+    async def my_async_test():
+        ...
+    ```
+    """
     return request.param
 
 
