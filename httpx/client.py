b1ec8c876e6a157e035dbf5e4a988647a21afd16
y.ohorodnik <y.ohorodnik@cleverdec.com>
2020-01-07 16:31:46 +0200

    linted with flake8

--- httpx/client.py
+++ httpx/client.py
@@ -679,9 +679,9 @@ class AsyncClient:
         while True:
             if len(history) > self.max_redirects:
                 raise TooManyRedirects()
-            if (request.method, request.url) in (
-                    (response.request.method, response.url)
-                    for response in history):
+            urls = ((response.request.method, response.url)
+                    for response in history)
+            if (request.method, request.url) in urls:
                 raise RedirectLoop()
 
             response = await self.send_handling_auth(
