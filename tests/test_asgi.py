c843a26b3d1a588df3018a45333544719bee4127
Kousik Mitra <kousikmitra@ITs-MacBook-Pro.local>
2020-03-22 15:40:45 +0530

    add type-check to test_asgi

--- tests/test_asgi.py
+++ tests/test_asgi.py
@@ -1,9 +1,11 @@
+from typing import Callable, Dict
+
 import pytest
 
 import httpx
 
 
-async def hello_world(scope, receive, send):
+async def hello_world(scope: Dict, receive: Callable, send: Callable) -> None:
     status = 200
     output = b"Hello, World!"
     headers = [(b"content-type", "text/plain"), (b"content-length", str(len(output)))]
@@ -12,7 +14,7 @@ async def hello_world(scope, receive, send):
     await send({"type": "http.response.body", "body": output})
 
 
-async def echo_body(scope, receive, send):
+async def echo_body(scope: Dict, receive: Callable, send: Callable) -> None:
     status = 200
     headers = [(b"content-type", "text/plain")]
 
@@ -25,11 +27,11 @@ async def echo_body(scope, receive, send):
         await send({"type": "http.response.body", "body": body, "more_body": more_body})
 
 
-async def raise_exc(scope, receive, send):
+async def raise_exc(scope: Dict, receive: Callable, send: Callable) -> None:
     raise ValueError()
 
 
-async def raise_exc_after_response(scope, receive, send):
+async def raise_exc_after_response(scope: Dict, receive: Callable, send: Callable) -> None:
     status = 200
     output = b"Hello, World!"
     headers = [(b"content-type", "text/plain"), (b"content-length", str(len(output)))]
@@ -40,7 +42,7 @@ async def raise_exc_after_response(scope, receive, send):
 
 
 @pytest.mark.asyncio
-async def test_asgi():
+async def test_asgi() -> None:
     client = httpx.AsyncClient(app=hello_world)
     response = await client.get("http://www.example.org/")
     assert response.status_code == 200
@@ -48,7 +50,7 @@ async def test_asgi():
 
 
 @pytest.mark.asyncio
-async def test_asgi_upload():
+async def test_asgi_upload() -> None:
     client = httpx.AsyncClient(app=echo_body)
     response = await client.post("http://www.example.org/", data=b"example")
     assert response.status_code == 200
@@ -56,14 +58,14 @@ async def test_asgi_upload():
 
 
 @pytest.mark.asyncio
-async def test_asgi_exc():
+async def test_asgi_exc() -> None:
     client = httpx.AsyncClient(app=raise_exc)
     with pytest.raises(ValueError):
         await client.get("http://www.example.org/")
 
 
 @pytest.mark.asyncio
-async def test_asgi_exc_after_response():
+async def test_asgi_exc_after_response() -> None:
     client = httpx.AsyncClient(app=raise_exc_after_response)
     with pytest.raises(ValueError):
         await client.get("http://www.example.org/")
