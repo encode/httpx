c9f1cdfa3d73881ccbf95ecacc52ba0ee01c6130
euri10 <benoit.barthelet@gmail.com>
2020-07-29 11:47:01 +0200

    Blind path fix

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -150,9 +150,7 @@ def test_get_ssl_cert_file():
     os.environ["SSL_CERT_FILE"] = str(TESTS_DIR / "test_utils.py")
     # SSL_CERT_FILE is correctly set, SSL_CERT_DIR is not set.
     ca_bundle = get_ca_bundle_from_env()
-    assert ca_bundle is not None and ca_bundle.endswith(
-        Path("tests/test_utils.py").as_posix()
-    )
+    assert ca_bundle is not None and ca_bundle.endswith(str(TESTS_DIR / "test_utils.py"))
 
     os.environ["SSL_CERT_FILE"] = "wrongfile"
     # SSL_CERT_FILE is set with wrong file,  SSL_CERT_DIR is not set.
