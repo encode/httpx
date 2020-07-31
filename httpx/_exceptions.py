48865746426f37cc5e53cac24e1bd91b0171aad0
Tom Christie <tom@tomchristie.com>
2020-07-31 09:57:03 +0100

    Update httpx/_exceptions.py
    Co-authored-by: Florimond Manca <florimond.manca@gmail.com>

--- httpx/_exceptions.py
+++ httpx/_exceptions.py
@@ -200,7 +200,7 @@ class CookieConflict(Exception):
     """
     Attempted to lookup a cookie by name, but multiple cookies existed.
 
-    Can occur when calling `response.cookies.get(...)`
+    Can occur when calling `response.cookies.get(...)`.
     """
 
     def __init__(self, message: str) -> None:
