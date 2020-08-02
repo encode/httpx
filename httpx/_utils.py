4b0aecc3397b9cd9a0949cabcc42999bce9f5602
johnj <10510431+j178@users.noreply.github.com>
2020-08-02 17:38:38 +0800

    Handle bare env proxy hostname gracefully

--- httpx/_utils.py
+++ httpx/_utils.py
@@ -311,7 +311,8 @@ def get_environment_proxies() -> typing.Dict[str, typing.Optional[str]]:
 
     for scheme in ("http", "https", "all"):
         if proxy_info.get(scheme):
-            mounts[scheme] = proxy_info[scheme]
+            hostname = proxy_info[scheme]
+            mounts[scheme] = hostname if "://" in hostname else f"http://{hostname}"
 
     no_proxy_hosts = [host.strip() for host in proxy_info.get("no", "").split(",")]
     for hostname in no_proxy_hosts:
