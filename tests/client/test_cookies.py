28c72050e0602481b294231fc9006609763cd612
Tom Christie <tom@tomchristie.com>
2020-08-26 14:10:23 +0100

    Better test case consistency. Prefer `import httpx` and `httpx.Client`. (#1222)
    * Prefer httpx.Client over httpx.AsyncClient in test cases, unless required.
    
    * Prefer httpx.Client in test_headers
    
    * Consistent httpx imports and httpx.Client usage
    
    * Use 'import httpx' consistently in tests. Prefer httpx.Client.

--- tests/client/test_cookies.py
+++ tests/client/test_cookies.py
@@ -2,9 +2,8 @@ import typing
 from http.cookiejar import Cookie, CookieJar
 
 import httpcore
-import pytest
 
-from httpx import AsyncClient, Cookies
+import httpx
 from httpx._content_streams import ByteStream, ContentStream, JSONStream
 
 
@@ -16,13 +15,13 @@ def get_header_value(headers, key, default=None):
     return default
 
 
-class MockTransport(httpcore.AsyncHTTPTransport):
-    async def request(
+class MockTransport(httpcore.SyncHTTPTransport):
+    def request(
         self,
         method: bytes,
         url: typing.Tuple[bytes, bytes, typing.Optional[int], bytes],
         headers: typing.List[typing.Tuple[bytes, bytes]] = None,
-        stream: httpcore.AsyncByteStream = None,
+        stream: httpcore.SyncByteStream = None,
         timeout: typing.Mapping[str, typing.Optional[float]] = None,
     ) -> typing.Tuple[
         bytes, int, bytes, typing.List[typing.Tuple[bytes, bytes]], ContentStream
@@ -41,23 +40,21 @@ class MockTransport(httpcore.AsyncHTTPTransport):
             raise NotImplementedError()  # pragma: no cover
 
 
-@pytest.mark.asyncio
-async def test_set_cookie() -> None:
+def test_set_cookie() -> None:
     """
     Send a request including a cookie.
     """
     url = "http://example.org/echo_cookies"
     cookies = {"example-name": "example-value"}
 
-    client = AsyncClient(transport=MockTransport())
-    response = await client.get(url, cookies=cookies)
+    client = httpx.Client(transport=MockTransport())
+    response = client.get(url, cookies=cookies)
 
     assert response.status_code == 200
     assert response.json() == {"cookies": "example-name=example-value"}
 
 
-@pytest.mark.asyncio
-async def test_set_cookie_with_cookiejar() -> None:
+def test_set_cookie_with_cookiejar() -> None:
     """
     Send a request including a cookie, using a `CookieJar` instance.
     """
@@ -85,15 +82,14 @@ async def test_set_cookie_with_cookiejar() -> None:
     )
     cookies.set_cookie(cookie)
 
-    client = AsyncClient(transport=MockTransport())
-    response = await client.get(url, cookies=cookies)
+    client = httpx.Client(transport=MockTransport())
+    response = client.get(url, cookies=cookies)
 
     assert response.status_code == 200
     assert response.json() == {"cookies": "example-name=example-value"}
 
 
-@pytest.mark.asyncio
-async def test_setting_client_cookies_to_cookiejar() -> None:
+def test_setting_client_cookies_to_cookiejar() -> None:
     """
     Send a request including a cookie, using a `CookieJar` instance.
     """
@@ -121,59 +117,56 @@ async def test_setting_client_cookies_to_cookiejar() -> None:
     )
     cookies.set_cookie(cookie)
 
-    client = AsyncClient(transport=MockTransport())
+    client = httpx.Client(transport=MockTransport())
     client.cookies = cookies  # type: ignore
-    response = await client.get(url)
+    response = client.get(url)
 
     assert response.status_code == 200
     assert response.json() == {"cookies": "example-name=example-value"}
 
 
-@pytest.mark.asyncio
-async def test_set_cookie_with_cookies_model() -> None:
+def test_set_cookie_with_cookies_model() -> None:
     """
     Send a request including a cookie, using a `Cookies` instance.
     """
 
     url = "http://example.org/echo_cookies"
-    cookies = Cookies()
+    cookies = httpx.Cookies()
     cookies["example-name"] = "example-value"
 
-    client = AsyncClient(transport=MockTransport())
-    response = await client.get(url, cookies=cookies)
+    client = httpx.Client(transport=MockTransport())
+    response = client.get(url, cookies=cookies)
 
     assert response.status_code == 200
     assert response.json() == {"cookies": "example-name=example-value"}
 
 
-@pytest.mark.asyncio
-async def test_get_cookie() -> None:
+def test_get_cookie() -> None:
     url = "http://example.org/set_cookie"
 
-    client = AsyncClient(transport=MockTransport())
-    response = await client.get(url)
+    client = httpx.Client(transport=MockTransport())
+    response = client.get(url)
 
     assert response.status_code == 200
     assert response.cookies["example-name"] == "example-value"
     assert client.cookies["example-name"] == "example-value"
 
 
-@pytest.mark.asyncio
-async def test_cookie_persistence() -> None:
+def test_cookie_persistence() -> None:
     """
     Ensure that Client instances persist cookies between requests.
     """
-    client = AsyncClient(transport=MockTransport())
+    client = httpx.Client(transport=MockTransport())
 
-    response = await client.get("http://example.org/echo_cookies")
+    response = client.get("http://example.org/echo_cookies")
     assert response.status_code == 200
     assert response.json() == {"cookies": None}
 
-    response = await client.get("http://example.org/set_cookie")
+    response = client.get("http://example.org/set_cookie")
     assert response.status_code == 200
     assert response.cookies["example-name"] == "example-value"
     assert client.cookies["example-name"] == "example-value"
 
-    response = await client.get("http://example.org/echo_cookies")
+    response = client.get("http://example.org/echo_cookies")
     assert response.status_code == 200
     assert response.json() == {"cookies": "example-name=example-value"}
--- tests/client/test_headers.py
+++ tests/client/test_headers.py
@@ -5,17 +5,17 @@ import typing
 import httpcore
 import pytest
 
-from httpx import AsyncClient, Headers, Request, __version__
+import httpx
 from httpx._content_streams import ContentStream, JSONStream
 
 
-class MockTransport(httpcore.AsyncHTTPTransport):
-    async def request(
+class MockTransport(httpcore.SyncHTTPTransport):
+    def request(
         self,
         method: bytes,
         url: typing.Tuple[bytes, bytes, typing.Optional[int], bytes],
         headers: typing.List[typing.Tuple[bytes, bytes]] = None,
-        stream: httpcore.AsyncByteStream = None,
+        stream: httpcore.SyncByteStream = None,
         timeout: typing.Mapping[str, typing.Optional[float]] = None,
     ) -> typing.Tuple[
         bytes, int, bytes, typing.List[typing.Tuple[bytes, bytes]], ContentStream
@@ -28,16 +28,15 @@ class MockTransport(httpcore.AsyncHTTPTransport):
         return b"HTTP/1.1", 200, b"OK", [], body
 
 
-@pytest.mark.asyncio
-async def test_client_header():
+def test_client_header():
     """
     Set a header in the Client.
     """
     url = "http://example.org/echo_headers"
     headers = {"Example-Header": "example-value"}
 
-    client = AsyncClient(transport=MockTransport(), headers=headers)
-    response = await client.get(url)
+    client = httpx.Client(transport=MockTransport(), headers=headers)
+    response = client.get(url)
 
     assert response.status_code == 200
     assert response.json() == {
@@ -47,18 +46,17 @@ async def test_client_header():
             "connection": "keep-alive",
             "example-header": "example-value",
             "host": "example.org",
-            "user-agent": f"python-httpx/{__version__}",
+            "user-agent": f"python-httpx/{httpx.__version__}",
         }
     }
 
 
-@pytest.mark.asyncio
-async def test_header_merge():
+def test_header_merge():
     url = "http://example.org/echo_headers"
     client_headers = {"User-Agent": "python-myclient/0.2.1"}
     request_headers = {"X-Auth-Token": "FooBarBazToken"}
-    client = AsyncClient(transport=MockTransport(), headers=client_headers)
-    response = await client.get(url, headers=request_headers)
+    client = httpx.Client(transport=MockTransport(), headers=client_headers)
+    response = client.get(url, headers=request_headers)
 
     assert response.status_code == 200
     assert response.json() == {
@@ -73,13 +71,12 @@ async def test_header_merge():
     }
 
 
-@pytest.mark.asyncio
-async def test_header_merge_conflicting_headers():
+def test_header_merge_conflicting_headers():
     url = "http://example.org/echo_headers"
     client_headers = {"X-Auth-Token": "FooBar"}
     request_headers = {"X-Auth-Token": "BazToken"}
-    client = AsyncClient(transport=MockTransport(), headers=client_headers)
-    response = await client.get(url, headers=request_headers)
+    client = httpx.Client(transport=MockTransport(), headers=client_headers)
+    response = client.get(url, headers=request_headers)
 
     assert response.status_code == 200
     assert response.json() == {
@@ -88,21 +85,20 @@ async def test_header_merge_conflicting_headers():
             "accept-encoding": "gzip, deflate, br",
             "connection": "keep-alive",
             "host": "example.org",
-            "user-agent": f"python-httpx/{__version__}",
+            "user-agent": f"python-httpx/{httpx.__version__}",
             "x-auth-token": "BazToken",
         }
     }
 
 
-@pytest.mark.asyncio
-async def test_header_update():
+def test_header_update():
     url = "http://example.org/echo_headers"
-    client = AsyncClient(transport=MockTransport())
-    first_response = await client.get(url)
+    client = httpx.Client(transport=MockTransport())
+    first_response = client.get(url)
     client.headers.update(
         {"User-Agent": "python-myclient/0.2.1", "Another-Header": "AThing"}
     )
-    second_response = await client.get(url)
+    second_response = client.get(url)
 
     assert first_response.status_code == 200
     assert first_response.json() == {
@@ -111,7 +107,7 @@ async def test_header_update():
             "accept-encoding": "gzip, deflate, br",
             "connection": "keep-alive",
             "host": "example.org",
-            "user-agent": f"python-httpx/{__version__}",
+            "user-agent": f"python-httpx/{httpx.__version__}",
         }
     }
 
@@ -129,13 +125,12 @@ async def test_header_update():
 
 
 def test_header_does_not_exist():
-    headers = Headers({"foo": "bar"})
+    headers = httpx.Headers({"foo": "bar"})
     with pytest.raises(KeyError):
         del headers["baz"]
 
 
-@pytest.mark.asyncio
-async def test_host_with_auth_and_port_in_url():
+def test_host_with_auth_and_port_in_url():
     """
     The Host header should only include the hostname, or hostname:port
     (for non-default ports only). Any userinfo or default port should not
@@ -143,8 +138,8 @@ async def test_host_with_auth_and_port_in_url():
     """
     url = "http://username:password@example.org:80/echo_headers"
 
-    client = AsyncClient(transport=MockTransport())
-    response = await client.get(url)
+    client = httpx.Client(transport=MockTransport())
+    response = client.get(url)
 
     assert response.status_code == 200
     assert response.json() == {
@@ -153,22 +148,21 @@ async def test_host_with_auth_and_port_in_url():
             "accept-encoding": "gzip, deflate, br",
             "connection": "keep-alive",
             "host": "example.org",
-            "user-agent": f"python-httpx/{__version__}",
+            "user-agent": f"python-httpx/{httpx.__version__}",
             "authorization": "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
         }
     }
 
 
-@pytest.mark.asyncio
-async def test_host_with_non_default_port_in_url():
+def test_host_with_non_default_port_in_url():
     """
     If the URL includes a non-default port, then it should be included in
     the Host header.
     """
     url = "http://username:password@example.org:123/echo_headers"
 
-    client = AsyncClient(transport=MockTransport())
-    response = await client.get(url)
+    client = httpx.Client(transport=MockTransport())
+    response = client.get(url)
 
     assert response.status_code == 200
     assert response.json() == {
@@ -177,12 +171,12 @@ async def test_host_with_non_default_port_in_url():
             "accept-encoding": "gzip, deflate, br",
             "connection": "keep-alive",
             "host": "example.org:123",
-            "user-agent": f"python-httpx/{__version__}",
+            "user-agent": f"python-httpx/{httpx.__version__}",
             "authorization": "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
         }
     }
 
 
 def test_request_auto_headers():
-    request = Request("GET", "https://www.example.org/")
+    request = httpx.Request("GET", "https://www.example.org/")
     assert "host" in request.headers
