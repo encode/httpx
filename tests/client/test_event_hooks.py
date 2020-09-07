2e62c971d5812175fa2ec578ad1697b157fac430
Tom Christie <tom@tomchristie.com>
2020-09-07 14:09:53 +0100

    Add test case to confirm that redirects/event hooks don't currently play together correctly

--- tests/client/test_event_hooks.py
+++ tests/client/test_event_hooks.py
@@ -1,23 +1,94 @@
+import typing
+
+import httpcore
 import pytest
 
 import httpx
 
 
-def test_event_hooks(server):
+class MockTransport(httpcore.SyncHTTPTransport):
+    def request(
+        self,
+        method: bytes,
+        url: typing.Tuple[bytes, bytes, typing.Optional[int], bytes],
+        headers: typing.List[typing.Tuple[bytes, bytes]] = None,
+        stream: httpcore.SyncByteStream = None,
+        timeout: typing.Mapping[str, typing.Optional[float]] = None,
+    ) -> typing.Tuple[
+        bytes,
+        int,
+        bytes,
+        typing.List[typing.Tuple[bytes, bytes]],
+        httpcore.SyncByteStream,
+    ]:
+        scheme, host, port, path = url
+        if path == b"/redirect":
+            status_code = 303
+            headers = [(b"server", b"testserver"), (b"location", b"/")]
+        elif path.startswith(b"/status/"):
+            status_code = int(path[-3:].decode("ascii"))
+            headers = [(b"server", b"testserver")]
+        else:
+            status_code = 200
+            headers = [(b"server", b"testserver")]
+
+        return (
+            b"HTTP/1.1",
+            status_code,
+            b"",
+            headers,
+            httpcore.PlainByteStream(b""),
+        )
+
+
+class AsyncMockTransport(httpcore.AsyncHTTPTransport):
+    async def request(
+        self,
+        method: bytes,
+        url: typing.Tuple[bytes, bytes, typing.Optional[int], bytes],
+        headers: typing.List[typing.Tuple[bytes, bytes]] = None,
+        stream: httpcore.AsyncByteStream = None,
+        timeout: typing.Mapping[str, typing.Optional[float]] = None,
+    ) -> typing.Tuple[
+        bytes,
+        int,
+        bytes,
+        typing.List[typing.Tuple[bytes, bytes]],
+        httpcore.AsyncByteStream,
+    ]:
+        scheme, host, port, path = url
+        if path == b"/redirect":
+            status_code = 303
+            headers = [(b"server", b"testserver"), (b"location", b"/")]
+        elif path.startswith(b"/status/"):
+            status_code = int(path[-3:].decode("ascii"))
+            headers = [(b"server", b"testserver")]
+        else:
+            status_code = 200
+            headers = [(b"server", b"testserver")]
+
+        return (
+            b"HTTP/1.1",
+            status_code,
+            b"",
+            headers,
+            httpcore.PlainByteStream(b""),
+        )
+
+
+def test_event_hooks():
     events = []
 
     def on_request(request):
         events.append({"event": "request", "headers": dict(request.headers)})
 
     def on_response(response):
-        headers = dict(response.headers)
-        headers["date"] = "Mon, 1 Jan 2020 12:34:56 GMT"
-        events.append({"event": "response", "headers": headers})
+        events.append({"event": "response", "headers": dict(response.headers)})
 
     event_hooks = {"request": [on_request], "response": [on_response]}
 
-    with httpx.Client(event_hooks=event_hooks) as http:
-        http.get(server.url, auth=("username", "password"))
+    with httpx.Client(event_hooks=event_hooks, transport=MockTransport()) as http:
+        http.get("http://127.0.0.1:8000/", auth=("username", "password"))
 
     assert events == [
         {
@@ -33,12 +104,7 @@ def test_event_hooks(server):
         },
         {
             "event": "response",
-            "headers": {
-                "date": "Mon, 1 Jan 2020 12:34:56 GMT",
-                "server": "uvicorn",
-                "content-type": "text/plain",
-                "transfer-encoding": "chunked",
-            },
+            "headers": {"server": "testserver"},
         },
     ]
 
@@ -49,30 +115,29 @@ def test_event_hooks_raising_exception(server):
 
     event_hooks = {"response": [raise_on_4xx_5xx]}
 
-    with httpx.Client(event_hooks=event_hooks) as http:
-        url = server.url.copy_with(path="/status/400")
+    with httpx.Client(event_hooks=event_hooks, transport=MockTransport()) as http:
         try:
-            http.get(url)
+            http.get("http://127.0.0.1:8000/status/400")
         except httpx.HTTPStatusError as exc:
             assert exc.response.is_closed
 
 
 @pytest.mark.usefixtures("async_environment")
-async def test_async_event_hooks(server):
+async def test_async_event_hooks():
     events = []
 
     async def on_request(request):
         events.append({"event": "request", "headers": dict(request.headers)})
 
     async def on_response(response):
-        headers = dict(response.headers)
-        headers["date"] = "Mon, 1 Jan 2020 12:34:56 GMT"
-        events.append({"event": "response", "headers": headers})
+        events.append({"event": "response", "headers": dict(response.headers)})
 
     event_hooks = {"request": [on_request], "response": [on_response]}
 
-    async with httpx.AsyncClient(event_hooks=event_hooks) as http:
-        await http.get(server.url, auth=("username", "password"))
+    async with httpx.AsyncClient(
+        event_hooks=event_hooks, transport=AsyncMockTransport()
+    ) as http:
+        await http.get("http://127.0.0.1:8000/", auth=("username", "password"))
 
     assert events == [
         {
@@ -88,26 +153,59 @@ async def test_async_event_hooks(server):
         },
         {
             "event": "response",
-            "headers": {
-                "date": "Mon, 1 Jan 2020 12:34:56 GMT",
-                "server": "uvicorn",
-                "content-type": "text/plain",
-                "transfer-encoding": "chunked",
-            },
+            "headers": {"server": "testserver"},
         },
     ]
 
 
 @pytest.mark.usefixtures("async_environment")
-async def test_async_event_hooks_raising_exception(server):
+async def test_async_event_hooks_raising_exception():
     async def raise_on_4xx_5xx(response):
         response.raise_for_status()
 
     event_hooks = {"response": [raise_on_4xx_5xx]}
 
-    async with httpx.AsyncClient(event_hooks=event_hooks) as http:
-        url = server.url.copy_with(path="/status/400")
+    async with httpx.AsyncClient(
+        event_hooks=event_hooks, transport=AsyncMockTransport()
+    ) as http:
         try:
-            await http.get(url)
+            await http.get("http://127.0.0.1:8000/status/400")
         except httpx.HTTPStatusError as exc:
             assert exc.response.is_closed
+
+
+def test_event_hooks_with_redirect():
+    """
+    A redirect request should not trigger a second 'request' event hook.
+    """
+
+    events = []
+
+    def on_request(request):
+        events.append({"event": "request", "headers": dict(request.headers)})
+
+    def on_response(response):
+        events.append({"event": "response", "headers": dict(response.headers)})
+
+    event_hooks = {"request": [on_request], "response": [on_response]}
+
+    with httpx.Client(event_hooks=event_hooks, transport=MockTransport()) as http:
+        http.get("http://127.0.0.1:8000/redirect", auth=("username", "password"))
+
+    assert events == [
+        {
+            "event": "request",
+            "headers": {
+                "host": "127.0.0.1:8000",
+                "user-agent": f"python-httpx/{httpx.__version__}",
+                "accept": "*/*",
+                "accept-encoding": "gzip, deflate, br",
+                "connection": "keep-alive",
+                "authorization": "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+            },
+        },
+        {
+            "event": "response",
+            "headers": {"server": "testserver"},
+        },
+    ]
