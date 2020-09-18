1acd370db7f96a2f54a071ca2a3ead4ccdd28242
Tom Christie <tom@tomchristie.com>
2020-09-19 00:09:10 +0100

    Update 'rich' package to version 7

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
     extras_require={
         "http2": "h2==3.*",
         "brotli": "brotlipy==0.7.*",
-        "cli": ["click==7.*", "rich==5.*"]
+        "cli": ["click==7.*", "rich==7.*"]
     },
     entry_points = {
         "console_scripts": "httpx=httpx._cli.main:main"
