78afd08e0fc198d05f95728011d1bc7c0c213b30
daa <aduryagin@gmail.com>
2020-09-24 10:42:26 +0300

    Properly close stream on async response close (#1316)

--- httpx/_client.py
+++ httpx/_client.py
@@ -1505,7 +1505,7 @@ class AsyncClient(BaseClient):
 
         async def on_close(response: Response) -> None:
             response.elapsed = datetime.timedelta(seconds=await timer.async_elapsed())
-            if hasattr(stream, "close"):
+            if hasattr(stream, "aclose"):
                 await stream.aclose()
 
         response = Response(
