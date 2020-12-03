1dc8dbff0ac7e550e2bec8abbebcf0e88847a3ea
Andres Alvarez <andresalvarez353@gmail.com>
2020-12-03 13:07:30 -0600

    Remove file

--- test.py
+++
@@ -1,8 +0,0 @@
-import httpx
-
-
-if __name__ == '__main__':
-    r = httpx.get('https://www.example.org/')
-    r.cookies.set(name="foo", value="bar", domain="http://blah.com")
-    r.cookies.set(name="fizz", value="buzz", domain="http://hello.com")
-    print(repr(r.cookies))
