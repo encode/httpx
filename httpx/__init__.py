02bc561775aaaf2fb3deaebc5f1b9f6cc0f1f9d2
Jaakko Lappalainen <jkk.lapp@gmail.com>
2021-04-08 21:54:06 +0000

    import variable in __init__

--- httpx/__init__.py
+++ httpx/__init__.py
@@ -1,7 +1,7 @@
 from .__version__ import __description__, __title__, __version__
 from ._api import delete, get, head, options, patch, post, put, request, stream
 from ._auth import Auth, BasicAuth, DigestAuth
-from ._client import AsyncClient, Client
+from ._client import USE_CLIENT_DEFAULT, AsyncClient, Client
 from ._config import Limits, Proxy, Timeout, create_ssl_context
 from ._exceptions import (
     CloseError,
@@ -102,6 +102,7 @@ __all__ = [
     "TooManyRedirects",
     "TransportError",
     "UnsupportedProtocol",
+    "USE_CLIENT_DEFAULT",
     "URL",
     "WriteError",
     "WriteTimeout",
