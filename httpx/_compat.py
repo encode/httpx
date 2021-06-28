7452bf15b535d9eb9be84e1cb3deb174cae861d9
Thomas Grainger <tagrain@gmail.com>
2021-06-28 13:15:26 +0100

    check sys.version_info and ssl.OPENSSL_VERSION_INFO once

--- httpx/_compat.py
+++ httpx/_compat.py
@@ -12,11 +12,11 @@ try:
 except ImportError:
     from async_generator import asynccontextmanager  # type: ignore # noqa
 
+if sys.version_info >= (3, 10) or (
+    sys.version_info >= (3, 7) and ssl.OPENSSL_VERSION_INFO >= (1, 1, 0, 7)
+):
 
-def set_minimum_tls_version_1_2(context: ssl.SSLContext) -> None:
-    if sys.version_info >= (3, 10) or (
-        sys.version_info >= (3, 7) and ssl.OPENSSL_VERSION_INFO >= (1, 1, 0, 7)
-    ):
+    def set_minimum_tls_version_1_2(context: ssl.SSLContext) -> None:
         # The OP_NO_SSL* and OP_NO_TLS* become deprecated in favor of
         # 'SSLContext.minimum_version' from Python 3.7 onwards, however
         # this attribute is not available unless the ssl module is compiled
@@ -24,7 +24,11 @@ def set_minimum_tls_version_1_2(context: ssl.SSLContext) -> None:
         # https://docs.python.org/3.10/library/ssl.html#ssl.SSLContext.minimum_version
         # https://docs.python.org/3.7/library/ssl.html#ssl.SSLContext.minimum_version
         context.minimum_version = ssl.TLSVersion.TLSv1_2
-    else:
+
+
+else:
+
+    def set_minimum_tls_version_1_2(context: ssl.SSLContext) -> None:
         # If 'minimum_version' isn't available, we configure these options with
         # the older deprecated variants.
         context.options |= ssl.OP_NO_SSLv2
