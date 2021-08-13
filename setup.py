d0ce237fb09da1502f89058a84cb555042be9e2e
Tom Christie <tom@tomchristie.com>
2021-08-13 11:00:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
         "async_generator; python_version < '3.7'"
     ],
     extras_require={
-        "http2": "httpcore[http2]",
+        "http2": "h2>=3,<5",
         "brotli": "brotlicffi==1.*",
     },
     classifiers=[
