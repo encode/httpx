1224935a6b52aec18f1634697f21578a1f7e4be2
Doug Addy <da1910@protonmail.com>
2021-08-27 16:49:47 +0100

    Remove ps1 scripts and simplify CI script

--- scripts/win/clean.ps1
+++
@@ -1,4 +0,0 @@
-Remove-Item ./dist -Recurse -ErrorAction Ignore
-Remove-Item ./site -Recurse -ErrorAction Ignore
-Remove-Item ./htmlcov -Recurse -ErrorAction Ignore
-Remove-Item ./httpx.egg-info -Recurse -ErrorAction Ignore
