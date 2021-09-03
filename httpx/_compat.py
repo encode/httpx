2e290808ba84b476c41bc982b21f68ed3eec4f60
Robert Craigie <robertcraigie30@gmail.com>
2021-09-03 14:40:03 +0100

    Fix conditional imports for pyright

--- httpx/_compat.py
+++ httpx/_compat.py
@@ -7,9 +7,9 @@ import sys
 
 # `contextlib.asynccontextmanager` exists from Python 3.7 onwards.
 # For 3.6 we require the `async_generator` package for a backported version.
-try:
+if sys.version_info >= (3, 7):
     from contextlib import asynccontextmanager  # type: ignore
-except ImportError:
+else:
     from async_generator import asynccontextmanager  # type: ignore # noqa
 
 # Brotli support is optional
