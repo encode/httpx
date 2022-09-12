d7d5ff41a100e4026dba3d9406645ed668a42266
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-09-12 13:52:35 -0500

    linting and fixes

--- httpx/_multipart.py
+++ httpx/_multipart.py
@@ -8,6 +8,7 @@ from ._types import (
     AsyncByteStream,
     FileContent,
     FileTypes,
+    RequestData,
     RequestFiles,
     SyncByteStream,
 )
@@ -190,7 +191,10 @@ class MultipartStream(SyncByteStream, AsyncByteStream):
     """
 
     def __init__(
-        self, data: dict, files: RequestFiles, boundary: typing.Optional[bytes] = None
+        self,
+        data: RequestData,
+        files: RequestFiles,
+        boundary: typing.Optional[bytes] = None,
     ) -> None:
         if boundary is None:
             boundary = binascii.hexlify(os.urandom(16))
@@ -202,7 +206,7 @@ class MultipartStream(SyncByteStream, AsyncByteStream):
         self.fields = list(self._iter_fields(data, files))
 
     def _iter_fields(
-        self, data: dict, files: RequestFiles
+        self, data: RequestData, files: RequestFiles
     ) -> typing.Iterator[typing.Union[FileField, DataField]]:
         for name, value in data.items():
             if isinstance(value, (tuple, list)):
