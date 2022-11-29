6d2ee61db33745bd37fbc4de103b4c64cd7d6e92
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-11-28 23:50:44 -0600

    remove type ignores that mypy doesn't need

--- httpx/_multipart.py
+++ httpx/_multipart.py
@@ -207,7 +207,7 @@ class MultipartStream(SyncByteStream, AsyncByteStream):
         self, data: RequestData, files: RequestFiles
     ) -> typing.Iterator[typing.Union[FileField, DataField]]:
         for name, data_value in data.items():
-            if not isinstance(name, str):  # type: ignore
+            if not isinstance(name, str):
                 raise TypeError(
                     "Invalid type for name."
                     f" Expected str, got {type(name)}: {name!r}"
@@ -221,7 +221,7 @@ class MultipartStream(SyncByteStream, AsyncByteStream):
                 data_value, (*PRIMITIVE_DATA_TYPES, bytes)
             ):
                 yield DataField(name=name, value=data_value)
-            elif isinstance(data_value, typing.Sequence):  # type: ignore
+            elif isinstance(data_value, typing.Sequence):
                 for item in data_value:
                     if not isinstance(item, (*PRIMITIVE_DATA_TYPES, bytes)):
                         raise err
