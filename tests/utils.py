049afe5b25399204a712e434c5c0bc937459b7fe
Martijn Pieters <mj@zopatista.com>
2022-11-30 09:04:54 +0000

    Typing: enable disallow_incomplete_defs (#2476)
    The only places mypy reports issues is in the test suite.

--- tests/utils.py
+++ tests/utils.py
@@ -1,12 +1,13 @@
 import contextlib
 import logging
 import os
+import typing
 
 from httpx import _utils
 
 
 @contextlib.contextmanager
-def override_log_level(log_level: str):
+def override_log_level(log_level: str) -> typing.Iterator[None]:
     os.environ["HTTPX_LOG_LEVEL"] = log_level
 
     # Force a reload on the logging handlers
