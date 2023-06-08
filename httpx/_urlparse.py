5bbacceb39d6faf885e4741633474853e6d1da43
Zanie <contact@zanie.dev>
2023-06-07 23:31:03 -0500

    Revert inclusion of "%"
    This is expected to fail tests due to double escaping

--- httpx/_urlparse.py
+++ httpx/_urlparse.py
@@ -261,10 +261,9 @@ def urlparse(url: str = "", **kwargs: typing.Optional[str]) -> ParseResult:
     parsed_path: str = quote(path, safe=SUB_DELIMS + ":/[]@")
     # For 'query' we need to drop '#' from the GEN_DELIMS set.
     # We also exclude '/' because it is more robust to replace it with a percent
-    # encoding despite it not being a requirement of the spec and include '%' to
-    # prevent duplicate encoding of previously quoted items.
+    # encoding despite it not being a requirement of the spec.
     parsed_query: typing.Optional[str] = (
-        None if query is None else quote(query, safe=SUB_DELIMS + ":?[]@%")
+        None if query is None else quote(query, safe=SUB_DELIMS + ":?[]@")
     )
     # For 'fragment' we can include all of the GEN_DELIMS set.
     parsed_fragment: typing.Optional[str] = (
