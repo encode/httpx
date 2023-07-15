9178458d41e2ced2302ed8c9180fbbb2e655b4ea
Trim21 <trim21.me@gmail.com>
2023-07-15 16:33:04 +0800

    lint

--- httpx/_models.py
+++ httpx/_models.py
@@ -711,7 +711,7 @@ class Response:
             and "Location" in self.headers
         )
 
-    def raise_for_status(self) -> 'Response':
+    def raise_for_status(self) -> "Response":
         """
         Raise the `HTTPStatusError` if one occurred.
         """
