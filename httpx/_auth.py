8302ab2765d2e7a4eb1caf5b430592bb9605ec92
Martin Musale <martinmusale@microsoft.com>
2023-09-13 22:55:05 +0300

    Fix linting error

--- httpx/_auth.py
+++ httpx/_auth.py
@@ -8,7 +8,7 @@ from base64 import b64encode
 from urllib.request import parse_http_list
 
 from ._exceptions import ProtocolError
-from ._models import Request, Response, Cookies
+from ._models import Cookies, Request, Response
 from ._utils import to_bytes, to_str, unquote
 
 if typing.TYPE_CHECKING:  # pragma: no cover
