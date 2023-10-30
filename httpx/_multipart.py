434f7b9753d7c00eb6a3f87463a8f689a2ddebe5
Tom Christie <tom@tomchristie.com>
2023-10-30 12:58:05 +0000

    Drop unneccessary binascii import

--- httpx/_multipart.py
+++ httpx/_multipart.py
@@ -1,4 +1,3 @@
-import binascii
 import io
 import os
 import typing
@@ -200,7 +199,7 @@ class MultipartStream(SyncByteStream, AsyncByteStream):
         boundary: typing.Optional[bytes] = None,
     ) -> None:
         if boundary is None:
-            boundary = binascii.hexlify(os.urandom(16))
+            boundary = ''.join([f'{byte:02x}' for byte in(os.urandom(16)])
 
         self.boundary = boundary
         self.content_type = "multipart/form-data; boundary=%s" % boundary.decode(
