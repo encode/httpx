c1bf6835fa147383a6267a6a30e57ad3c490c15e
Kar Petrosyan <92274156+karpetrosyan@users.noreply.github.com>
2024-02-21 23:03:17 +0400

    Update tests/test_exported_members.py

--- tests/test_exported_members.py
+++ tests/test_exported_members.py
@@ -3,16 +3,6 @@ import httpx
 
 def test_all_imports_are_exported() -> None:
     included_private_members = ["__description__", "__title__", "__version__"]
-    print(
-        sorted(
-            (
-                member
-                for member in vars(httpx).keys()
-                if not member.startswith("_") or member in included_private_members
-            ),
-            key=str.casefold,
-        )
-    )
     assert httpx.__all__ == sorted(
         (
             member
