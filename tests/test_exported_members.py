bd2448cbf6658c9d3a100486cd3e223db149351d
Tom Christie <tom@tomchristie.com>
2024-10-25 12:07:53 +0100

    Structure imports in __init__.py

--- tests/test_exported_members.py
+++ tests/test_exported_members.py
@@ -3,11 +3,10 @@ import httpx
 
 def test_all_imports_are_exported() -> None:
     included_private_members = ["__description__", "__title__", "__version__"]
-    assert httpx.__all__ == sorted(
+    assert sorted(httpx.__all__) == sorted(
         (
             member
             for member in vars(httpx).keys()
             if not member.startswith("_") or member in included_private_members
         ),
-        key=str.casefold,
     )
