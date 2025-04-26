cffc78b90b65c52f2eb25805bf19b497831dea5e
Alyssa Coghlan <ncoghlan@gmail.com>
2025-04-27 01:21:27 +1000

    No exception raised until Python 3.14 is tested

--- httpx/__init__.py
+++ httpx/__init__.py
@@ -104,7 +104,7 @@ for __name in __all__:
     if not __name.startswith("__"):
         try:
             setattr(__locals[__name], "__module__", "httpx")  # noqa
-        except AttributeError:
+        except AttributeError:  # noqa
             # typing.Union instances are immutable in Python 3.14+,
             # so don't fail if writing to "__module__" fails
-            continue
+            continue  # noqa
