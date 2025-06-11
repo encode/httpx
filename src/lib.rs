ee73482f38ce177b9b9cbbc29a89774517574e4d
Бакыт Ниязалиев <niyazaliev@sputnikfund.ru>
2025-06-11 22:44:12 +0700

    feat: add urlparse module and integrate with py_module

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,3 @@
 mod py_module;
+mod urlparse;
+
--- src/py_module.rs
+++ src/py_module.rs
@@ -1,4 +1,12 @@
 use pyo3::prelude::*;
 
 #[pymodule(gil_used = false)]
-mod _httpx {}
+mod _httpx {
+    use super::*;
+
+    #[pymodule]
+    mod urlparse {
+        #[pymodule_export]
+        use crate::urlparse::ParseResult;
+    }
+}
